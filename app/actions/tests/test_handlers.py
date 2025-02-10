009df617cb23831196e0b2fccf06a94956f7effd
Victor H. García <victorhugogarcia13d@gmail.com>
2025-02-10 13:53:23 -0600

    Mock `publish_event`

--- app/actions/tests/test_handlers.py
+++ app/actions/tests/test_handlers.py
@@ -27,7 +27,7 @@ async def test_action_auth_bad_credentials(mocker):
     assert result == {"valid_credentials": False, "message": "Bad credentials"}
 
 @pytest.mark.asyncio
-async def test_action_pull_observations_triggers_playback_action(mocker, integration_v2):
+async def test_action_pull_observations_triggers_playback_action(mocker, integration_v2, mock_publish_event):
     mocker.patch("app.actions.client.get_token", return_value="fake_token")
     mocker.patch("app.actions.client.get_devices", return_value=[
         DeviceResponse.parse_obj({"imei": "12345", "devicename": "device"})
@@ -35,6 +35,8 @@ async def test_action_pull_observations_triggers_playback_action(mocker, integra
     mock_now = mocker.patch("app.actions.handlers.datetime")
     mock_now.now.return_value = datetime.now(timezone.utc)
     mocker.patch("app.services.state.IntegrationStateManager.get_state", return_value=None)
+    mocker.patch("app.services.activity_logger.publish_event", mock_publish_event)
+    mocker.patch("app.services.action_runner.publish_event", mock_publish_event)
     mocker.patch("app.services.action_scheduler.trigger_action", return_value=None)
     mock_trigger_action = mocker.patch("app.services.action_runner.execute_action", return_value=None)
 
@@ -63,9 +65,11 @@ async def test_action_pull_observations_triggers_playback_action(mocker, integra
     )
 
 @pytest.mark.asyncio
-async def test_action_pull_observations_no_devices(mocker, integration_v2):
+async def test_action_pull_observations_no_devices(mocker, integration_v2, mock_publish_event):
     mocker.patch("app.actions.client.get_token", return_value="fake_token")
     mocker.patch("app.actions.client.get_devices", return_value=[])
+    mocker.patch("app.services.activity_logger.publish_event", mock_publish_event)
+    mocker.patch("app.services.action_runner.publish_event", mock_publish_event)
     integration = integration_v2
 
     # Modify auth config
@@ -77,8 +81,10 @@ async def test_action_pull_observations_no_devices(mocker, integration_v2):
     assert result == {"devices_triggered": 0}
 
 @pytest.mark.asyncio
-async def test_action_pull_observations_auth_failure(mocker, integration_v2):
+async def test_action_pull_observations_auth_failure(mocker, integration_v2, mock_publish_event):
     mocker.patch("app.actions.client.get_token", side_effect=Exception("Auth failed"))
+    mocker.patch("app.services.activity_logger.publish_event", mock_publish_event)
+    mocker.patch("app.services.action_runner.publish_event", mock_publish_event)
     integration = integration_v2
 
     # Modify auth config
@@ -92,6 +98,7 @@ async def test_action_pull_observations_auth_failure(mocker, integration_v2):
 async def test_action_playback_extracts_observations(
         mocker,
         integration_v2,
+        mock_publish_event,
         mock_gundi_client_v2_class,
         mock_gundi_sensors_client_class,
         mock_get_gundi_api_key
@@ -99,6 +106,8 @@ async def test_action_playback_extracts_observations(
     mocker.patch("app.actions.client.get_playback_observations", return_value=[
         PlaybackResponse.parse_obj({"gpstime": datetime.now(timezone.utc), "latitude": 0, "longitude": 0, "speed": 0, "course": 0})
     ])
+    mocker.patch("app.services.activity_logger.publish_event", mock_publish_event)
+    mocker.patch("app.services.action_runner.publish_event", mock_publish_event)
     mocker.patch("app.services.utils.generate_batches", return_value=[[{"name": "device", "source": "12345", "type": "tracking-device", "subject_type": "vehicle", "recorded_at": datetime.now(timezone.utc), "location": {"lat": 0, "lon": 0}, "additional": {"speed": 0, "course": 0}}]])
     mocker.patch("app.services.gundi.GundiClient", mock_gundi_client_v2_class)
     mocker.patch("app.services.gundi.GundiDataSenderClient", mock_gundi_sensors_client_class)
@@ -112,8 +121,10 @@ async def test_action_playback_extracts_observations(
     assert result == {"observations_extracted": 2}
 
 @pytest.mark.asyncio
-async def test_action_playback_no_observations(mocker, integration_v2):
+async def test_action_playback_no_observations(mocker, integration_v2, mock_publish_event):
     mocker.patch("app.actions.client.get_playback_observations", return_value=[])
+    mocker.patch("app.services.activity_logger.publish_event", mock_publish_event)
+    mocker.patch("app.services.action_runner.publish_event", mock_publish_event)
 
     integration = integration_v2
     action_config = PlaybackConfig(access_token="fake_token", device_info={"imei": "12345"}, imei="12345", begintime=0, endtime=0)
@@ -122,8 +133,10 @@ async def test_action_playback_no_observations(mocker, integration_v2):
     assert result == {"observations_extracted": 0}
 
 @pytest.mark.asyncio
-async def test_action_playback_http_error(mocker, integration_v2):
+async def test_action_playback_http_error(mocker, integration_v2, mock_publish_event):
     mocker.patch("app.actions.client.get_playback_observations", side_effect=httpx.HTTPStatusError("Error", request=None, response=None))
+    mocker.patch("app.services.activity_logger.publish_event", mock_publish_event)
+    mocker.patch("app.services.action_runner.publish_event", mock_publish_event)
 
     integration = integration_v2
     action_config = PlaybackConfig(access_token="fake_token", device_info={"imei": "12345"}, imei="12345", begintime=0, endtime=0)
